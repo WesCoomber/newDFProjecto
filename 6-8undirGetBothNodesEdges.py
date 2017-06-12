064453c4c773ae8b76bd285b30a8244513191fc8
Wesley Coomber <wcoomber@umich.edu>
2017-06-12 13:11:25 -0400

    start of afternoon monday

--- 6-8undirGetBothNodesEdges.py
+++ 6-8undirGetBothNodesEdges.py
@@ -78,7 +78,49 @@ while (flagEnterKeys == 1):
     else :
         print("ERROR! Please enter in a valid key for the instrNodes, instrEdges dictionary.")  
 
-    
+
+
+#register list from PIN
+#Make it more complete as we need
+#dictionary in python
+regMap = {
+        "(3,4)":"edi",
+        "(3,2)":"di",
+        "(4,4)":"esi",
+        "(4,2)":"si",
+        "(5,4)":"ebp",
+        "(5,2)":"bp",
+        "(6,4)":"esp",
+        "(6,2)":"sp",
+        "(7,4)":"ebx",
+        "(7,2)":"bx",
+        "(7,1)":"bl",
+        "(7,-1)":"bh",
+        "(8,4)":"edx",
+        "(8,2)":"dx",
+        "(8,1)":"dl",
+        "(8,-1)":"dh",
+        "(9,4)":"ecx",
+        "(9,2)":"cx",
+        "(9,1)":"cl",
+        "(9,-1)":"ch",
+        "(10,4)":"eax",
+        "(10,2)":"ax",
+        "(10,1)":"al",
+        "(10,-1)":"ah",
+        "(54,16)":"xmm0",
+        "(55,16)":"xmm1",
+        "(56,16)":"xmm2",
+        "(57,16)":"xmm3",
+        "(58,16)":"xmm4",
+        "(59,16)":"xmm5",
+        "(60,16)":"xmm6",
+        "(61,16)":"xmm7"
+}
+
+#sampleStr = '(9,1)'
+#print('regMap ' + sampleStr + ' ' + str(regMap[sampleStr]))
+
     
 ##New Graphviz-dot code here 
 graph = functools.partial(gv.Graph, format='svg')
@@ -799,8 +841,6 @@ for idx, c in enumerate(instrEdges):
 
 
 add_nodes(datG)
-#add_edges(datG)
-
 print(datG.source)
 
 with open(outFileName, 'w') as outFile:
